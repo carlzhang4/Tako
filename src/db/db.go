366db3fb0f21d5907dc6c1d95a547ee753846c4e
昵羽 <niyu.zj@alibaba-inc.com>
2019-07-09 11:09:07 +0800

    first

---
+++ src/db/db.go
@@ -0,0 +1,213 @@
+package db
+
+import(
+    "database/sql"
+    "fmt"
+    _"github.com/go-sql-driver/mysql"
+    "strconv"
+)
+
+
+
+type UserInfo struct{
+    UserName string
+    UserPhone string
+    UserId int
+}
+type Contract struct{
+    Name string
+    UserID int
+    Price string
+    Description string
+    Content string
+    ID int
+
+}
+
+func SignUp(name string,phone string,password string) int{
+	db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+    checkErr(err)
+
+    rows, err := db.Query("SELECT * FROM users WHERE name = ? or phone = ?", name,phone)
+    for rows.Next() {
+        return 1
+    }
+
+	stmt, err := db.Prepare("INSERT users SET name=?,phone=?,password=?,money=100")
+    checkErr(err)
+    res, err := stmt.Exec(name,phone,password)
+    checkErr(err)
+    afctedRows,err := res.RowsAffected()
+    checkErr(err)
+    fmt.Println(afctedRows)
+    return 0;
+}
+func QueryUser(name string,phone string)UserInfo{
+    db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+	checkErr(err)
+    rows, err := db.Query("SELECT name,phone,id FROM users WHERE name = ? and phone = ?", name,phone)
+    var resName string
+    var resPhone string
+    var resId int
+    for rows.Next() {
+        rows.Scan(&resName,&resPhone,&resId)
+        return UserInfo{resName,resPhone,resId}
+    }
+    return UserInfo{}
+}
+
+func SignIn(phone string,password string)UserInfo{
+    db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+	checkErr(err)
+    rows, err := db.Query("SELECT name,phone,id FROM users WHERE phone = ? and password = ?", phone,password)
+    var resName string
+    var resPhone string
+    var resId int
+    for rows.Next() {
+        rows.Scan(&resName,&resPhone,&resId)
+        return UserInfo{resName,resPhone,resId}
+    }
+    return UserInfo{"","",-1}
+}
+
+func UploadUserContract(name string,userID int,price string,description string,content string)int{
+    db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+    checkErr(err)
+
+    rows, err := db.Query("SELECT * FROM contract WHERE name = ? and userID = ?", name,userID)
+    for rows.Next() {
+        return 1
+    }
+    stmt, err := db.Prepare("INSERT contract SET name=?,userID=?,price=?,description=?,content=?")
+    checkErr(err)
+    res, err := stmt.Exec(name,userID,price,description,content)
+    checkErr(err)
+    afctedRows,err := res.RowsAffected()
+    checkErr(err)
+    fmt.Println(afctedRows)
+    return 0
+}
+
+func UploadMarketContract(name string,userID int,price string,description string,content string)int{
+    db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+    checkErr(err)
+
+    rows, err := db.Query("SELECT * FROM market WHERE name = ? and userID = ?", name,userID)
+    for rows.Next() {
+        fmt.Println("1111")
+        return 1
+    }
+    fmt.Println("2222")
+    stmt, err := db.Prepare("INSERT market SET name=?,userID=?,price=?,description=?,content=?")
+    checkErr(err)
+    res, err := stmt.Exec(name,userID,price,description,content)
+    checkErr(err)
+    afctedRows,err := res.RowsAffected()
+    checkErr(err)
+    fmt.Println(afctedRows)
+    return 0
+}
+
+func GetUserContract(userID int) []Contract{
+    db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+    checkErr(err)
+    fmt.Println(userID)
+    rows, err := db.Query("SELECT name,price,description,content,id FROM contract WHERE userID = ?", userID)
+
+    var name string
+    var price string
+    var description string
+    var content string
+    var id int
+    var contracts []Contract
+
+    for rows.Next() {
+        rows.Scan(&name,&price,&description,&content,&id)
+        fmt.Println(contracts)
+        contracts = append(contracts,Contract{name,userID,price,description,content,id})
+    }
+    return contracts
+}
+
+func GetMarketContract(userID int) []Contract{
+    db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+    checkErr(err)
+    rows, err := db.Query("SELECT name,price,description,content,id FROM market WHERE userID = ?", userID)
+
+    var name string
+    var price string
+    var description string
+    var content string
+    var id int
+    var contracts []Contract
+
+    for rows.Next() {
+        rows.Scan(&name,&price,&description,&content,&id)
+        fmt.Println(contracts)
+        contracts = append(contracts,Contract{name,userID,price,description,content,id})
+    }
+    return contracts
+}
+func BuyContract(userID int,contractID int)int{
+    db, err := sql.Open("mysql", "root:19050817@/designer?charset=utf8")
+    checkErr(err)
+    rows, err := db.Query("SELECT name,price,userID,description,content FROM market WHERE id = ?", contractID)
+    var name string
+    var price string
+    var sellerID int
+    var description string
+    var content string
+
+    var money string
+    for rows.Next(){
+        rows.Scan(&name,&price,&sellerID,&description,&content)
+    }
+    fmt.Println(userID,contractID)
+    if sellerID <=0{
+        return 1
+    }
+    rows, err = db.Query("SELECT * FROM contract WHERE name = ? and userID = ?", name,userID)
+    for rows.Next(){
+        return 1
+    }
+    rows,err = db.Query("SELECT money FROM users WHERE id = ?",userID)
+    for rows.Next(){
+        rows.Scan(&money)
+    }
+    moneynum,err := strconv.ParseFloat(money,64)
+    checkErr(err)
+    pricenum,err := strconv.ParseFloat(price,64)
+    checkErr(err)
+    if moneynum<pricenum{
+        return 1
+    }else{
+        stmt, err := db.Prepare("INSERT contract SET name=?,userID=?,price=?,description=?,content=?")
+        checkErr(err)
+        _, err = stmt.Exec(name,userID,price,description,content)
+        checkErr(err)
+
+        stmt, err = db.Prepare("UPDATE users SET money=? where id = ?")
+        checkErr(err)
+        _, err = stmt.Exec(moneynum-pricenum,userID)
+        checkErr(err)
+
+        rows,err = db.Query("SELECT money FROM users WHERE id = ?",sellerID)
+        for rows.Next(){
+            rows.Scan(&money)
+        }
+        moneynum,err := strconv.ParseFloat(money,64)
+        checkErr(err)
+
+        stmt, err = db.Prepare("UPDATE users SET money=? where id = ?")
+        checkErr(err)
+        _, err = stmt.Exec(moneynum+pricenum,sellerID)
+        checkErr(err)
+        return 0
+    }
+}
+
+func checkErr(err error) {
+    if err != nil {
+        fmt.Println(err)
+    }
+}
---
+++ src/main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"fmt"
+	"db"
+	"encoding/json"
+	"strconv"
+
+)
+func main(){
+	engine := gin.Default()
+	engine.POST("/test",test)
+	engine.POST("/auth/signup",signup)
+	engine.POST("/auth/signin",signin)
+	engine.POST("/fnc/getusercontract",getusercontract)
+	engine.POST("/fnc/postusercontract",postusercontract)
+	engine.POST("/fnc/getmarketcontract",getmarketcontract)
+	engine.POST("/fnc/postmarketcontract",postmarketcontract)
+	engine.POST("/fnc/buycontract",buycontract)
+	engine.Run(":8080")
+}
+
+func signup(c *gin.Context){
+	name := c.PostForm("userName")
+	phone := c.PostForm("userPhone")
+	password := c.PostForm("userPsw")
+
+	res := db.SignUp(name,phone,password)
+	if res == 0{
+		user := db.QueryUser(name,phone)
+		m,_ := json.Marshal(user)
+		c.JSON(http.StatusOK,gin.H{
+			"status":"0",
+			"msg":"SUCCESS",
+			"userInfo":string(m),
+			"token":"sodfmamdakfp34k3pk2p42",
+		},)
+	}else{
+		c.JSON(http.StatusOK,gin.H{
+			"status":"1",
+			"msg":"Signup Failed",
+		},)
+	}
+}
+
+func signin(c *gin.Context){
+	phone := c.PostForm("userPhone")
+	password := c.PostForm("userPsw")
+	user := db.SignIn(phone,password)
+	if user.UserId == -1{
+		c.JSON(http.StatusOK,gin.H{
+			"status":"1",
+			"msg":"Signin Failed",
+		},)
+	}else{
+		m,_ := json.Marshal(user)
+		c.JSON(http.StatusOK,gin.H{
+			"status":"0",
+			"msg":"SUCCESS",
+			"userInfo":string(m),
+			"token":"sodfmamdakfp34k3pk2p42",
+		},)
+	}
+}
+
+func getusercontract(c *gin.Context){
+	userIdString := c.PostForm("userId")
+	userId,_ := strconv.Atoi(userIdString)
+	contracts := db.GetUserContract(userId)
+
+	m,_ := json.Marshal(contracts)
+	c.JSON(http.StatusOK,gin.H{
+		"status":"0",
+		"msg":"SUCCESS",
+		"contracts":string(m),
+		"token":"sodfmamdakfp34k3pk2p42",
+	},)
+}
+
+func postusercontract(c *gin.Context){
+	userIdString := c.PostForm("userId")
+	userId,_ := strconv.Atoi(userIdString)
+	contractName := c.PostForm("contractName")
+	contractPrice := c.PostForm("contractPrice")
+	contractDesc := c.PostForm("contractDesc")
+	contractCont := c.PostForm("contractCont")
+	res := db.UploadUserContract(contractName,userId,contractPrice,contractDesc,contractCont)
+	if res == 0{
+		c.JSON(http.StatusOK,gin.H{
+			"status":"0",
+			"msg":"SUCCESS",
+		},)
+	}else{
+		c.JSON(http.StatusOK,gin.H{
+			"status":"1",
+			"msg":"PostContract Failed",
+		},)
+	}
+
+}
+
+func getmarketcontract(c *gin.Context){
+	userIdString := c.PostForm("userId")
+	userId,_ := strconv.Atoi(userIdString)
+	contracts := db.GetMarketContract(userId)
+
+	m,_ := json.Marshal(contracts)
+	c.JSON(http.StatusOK,gin.H{
+		"status":"0",
+		"msg":"SUCCESS",
+		"contracts":string(m),
+		"token":"sodfmamdakfp34k3pk2p42",
+	},)
+}
+
+func postmarketcontract(c *gin.Context){
+	userIdString := c.PostForm("userId")
+	userId,_ := strconv.Atoi(userIdString)
+	contractName := c.PostForm("contractName")
+	contractPrice := c.PostForm("contractPrice")
+	contractDesc := c.PostForm("contractDesc")
+	contractCont := c.PostForm("contractCont")
+	res := db.UploadUserContract(contractName,userId,contractPrice,contractDesc,contractCont)
+	if res == 0{
+		db.UploadMarketContract(contractName,userId,contractPrice,contractDesc,contractCont)
+		c.JSON(http.StatusOK,gin.H{
+			"status":"0",
+			"msg":"SUCCESS",
+		},)
+	}else{
+		c.JSON(http.StatusOK,gin.H{
+			"status":"1",
+			"msg":"PostMarketContract Failed",
+		},)
+	}
+}
+
+func buycontract(c *gin.Context){
+	userIdString := c.PostForm("userId")
+	userId,_ := strconv.Atoi(userIdString)
+	contractIdString := c.PostForm("contractId")
+	contractId,_ := strconv.Atoi(contractIdString)
+	res := db.BuyContract(userId,contractId)
+	if res == 0{
+		c.JSON(http.StatusOK,gin.H{
+			"status":"0",
+			"msg":"SUCCESS",
+		},)
+	}else{
+		c.JSON(http.StatusOK,gin.H{
+			"status":"1",
+			"msg":"BuyContract Failed",
+		},)
+	}
+}
+
+
+func test(c *gin.Context){
+	bookname := c.PostForm("bookname")
+	fmt.Println(bookname)
+	c.JSON(http.StatusOK,gin.H{
+		"books":"123",
+	},
+	)
+}
